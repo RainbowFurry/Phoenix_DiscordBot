446c0292df5d718d406ba32d7bc39e471e4e2c9b
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2024-12-30 17:35:20 +0100

    Added Bot Emojis

---
+++ src/main/java/net/rainbowfurry/phoenixbot/builder/BotEmojis.java
@@ -0,0 +1,11 @@
+package net.rainbowfurry.phoenixbot.builder;
+
+public class BotEmojis {
+
+    private static final String phoenix = ":Phoenix:";
+    private static final String phoenixMC = ":Phoenix_MC:";
+
+    private static final String phoenix_raw = "<:Phoenix:1323131576250535969>";
+    private static final String phoenixMC_raw = "<:Phoenix_MC:1323131349737017406>";
+
+}
