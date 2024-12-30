61ae2ef67ad325cce4f3a8c118eacfa393dd3535
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2024-12-30 13:17:14 +0100

    Complete User Info
    Template Structure

--- src/main/java/net/rainbowfurry/phoenixbot/listener/essentials/interaction/UserInteractionListener.java
+++ src/main/java/net/rainbowfurry/phoenixbot/listener/essentials/interaction/UserInteractionListener.java
@@ -2,13 +2,39 @@ package net.rainbowfurry.phoenixbot.listener.essentials.interaction;
 
 import net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.rainbowfurry.phoenixbot.commands.info.UserInfo;
 
 public class UserInteractionListener extends ListenerAdapter {
 
     @Override
     public void onUserContextInteraction(UserContextInteractionEvent event) {
-        if (event.getName().equals("Get user avatar")) {
-            event.reply("Avatar: " + event.getTarget().getEffectiveAvatarUrl()).queue();
+        switch (event.getName()) {
+
+            case "UserInfo":
+                final UserInfo userInfo = new UserInfo();
+                userInfo.displayUserInfo(event.getTargetMember(), event.getMessageChannel());
+                break;
+
+            case "Kick":
+                //
+                break;
+
+            case "Warn":
+                //
+                break;
+
+            case "Ban":
+                //
+                break;
+
+            case "Mute":
+                //
+                break;
+
+            case "Mod":
+                //
+                break;
+
         }
     }
 
