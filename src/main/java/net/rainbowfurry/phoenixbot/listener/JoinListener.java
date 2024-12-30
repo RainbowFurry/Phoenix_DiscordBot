eae62be32e83503d1b27e502c1fdb14f551a9baf
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2024-12-30 13:19:27 +0100

    first basic Version of Join and Leave Message.

--- src/main/java/net/rainbowfurry/phoenixbot/listener/JoinListener.java
+++ src/main/java/net/rainbowfurry/phoenixbot/listener/JoinListener.java
@@ -1,12 +1,31 @@
 package net.rainbowfurry.phoenixbot.listener;
 
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.rainbowfurry.phoenixbot.builder.CustomEmbedBuilder;
+import net.rainbowfurry.phoenixbot.core.CommandParser;
+
+import java.awt.*;
 
 public class JoinListener extends ListenerAdapter {
 
+    private final CustomEmbedBuilder customEmbedBuilder = new CustomEmbedBuilder();
+
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+
+        Member member = event.getMember();
+
+        // First Join - Yellow if enabled
+        //ustomEmbedBuilder.setEmbedColor(Color.YELLOW).setFooter(member.getEffectiveName() + " has joined the Server for the first Time!", member.getEffectiveAvatarUrl());
+
+        // Join - Green
+        customEmbedBuilder.setEmbedColor(Color.GREEN).setFooter(member.getEffectiveName() + " has joined the Server! Welcome back!", member.getEffectiveAvatarUrl());
+        //customEmbedBuilder.setEmbedColor(Color.GREEN).setImage(member.getEffectiveAvatarUrl()).setContent(member.getAsMention() + "has joined the Server!");
+        //customEmbedBuilder.setEmbedColor(Color.GREEN).setThumbnail(member.getEffectiveAvatarUrl()).setContent(member.getAsMention() + "has joined the Server!");
+        if(event.getGuild().getDefaultChannel() != null)
+            event.getGuild().getDefaultChannel().asTextChannel().sendMessageEmbeds(customEmbedBuilder.build()).queue();
 
     }
 
--- src/main/java/net/rainbowfurry/phoenixbot/listener/LeaveListener.java
+++ src/main/java/net/rainbowfurry/phoenixbot/listener/LeaveListener.java
@@ -1,8 +1,11 @@
 package net.rainbowfurry.phoenixbot.listener;
 
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.rainbowfurry.phoenixbot.builder.CustomEmbedBuilder;
+
+import java.awt.*;
 
 public class LeaveListener extends ListenerAdapter {
 
@@ -10,6 +13,13 @@ public class LeaveListener extends ListenerAdapter {
 
     @Override
     public void onGuildMemberRemove(GuildMemberRemoveEvent event) {
+
+        Member member = event.getMember();
+
+        customEmbedBuilder.setEmbedColor(Color.RED).setFooter(member.getEffectiveName() + " has left the Server!", member.getEffectiveAvatarUrl());
+
+        if(event.getGuild().getDefaultChannel() != null)
+            event.getGuild().getDefaultChannel().asTextChannel().sendMessageEmbeds(customEmbedBuilder.build()).queue();
 
     }
 
