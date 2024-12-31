937611fdd84fc3ab306ba52106c3824a5c23b9c9
Jasmin Hoffmannn <jasmin98.hoffmann@gmail.com>
2024-12-31 01:37:21 +0100

    Update own Log Functions

--- src/main/java/net/rainbowfurry/phoenixbot/manager/LogManager.java
+++ src/main/java/net/rainbowfurry/phoenixbot/manager/LogManager.java
@@ -9,14 +9,14 @@ import java.util.Date;
 public class LogManager {
 
     //ToDo Rework just the first basic Version for fast testing
-    private String _dir = "Log";
-
+    // In config file if multi or single log - eine file für ein tag, oder pro restart
+    //In config Log Format.
+    private final String _dir = "Log";
     private File _file;
-
-    private String LogString = "%Date% | %LogType% | %Priority% | %Content%";
+    private final String LogString = "%Date% | %LogType% | %Priority% | %Content%";
 
     public LogManager() {
-        this._file = new File(this._dir, (String.valueOf(new Date()) + ".log").replaceAll(" ", "_").replace(":", "-"));
+        this._file = new File(this._dir, ((new Date()) + ".log").replaceAll(" ", "_").replace(":", "-"));
         File dir = new File(this._dir);
         if (!dir.exists())
             dir.mkdirs();
@@ -28,12 +28,61 @@ public class LogManager {
             }
     }
 
+    public void Log(String content){
+        String _content = this.LogString.replace("%Date%", (new Date()).toString()).replace("%LogType%", LogType.SYSTEM.toString()).replace("%Priority%", String.valueOf(1)).replace("%Content%", content);
+        save(_content);
+    }
+
+    public void Info(String content){
+        String _content = this.LogString.replace("%Date%", (new Date()).toString()).replace("%LogType%", LogType.INFO.toString()).replace("%Priority%", String.valueOf(1)).replace("%Content%", content);
+        save(_content);
+    }
+
+    public void Debug(String content){
+        String _content = this.LogString.replace("%Date%", (new Date()).toString()).replace("%LogType%", LogType.DEBUG.toString()).replace("%Priority%", String.valueOf(1)).replace("%Content%", content);
+        save(_content);
+    }
+
+    public void System(String content){
+        String _content = this.LogString.replace("%Date%", (new Date()).toString()).replace("%LogType%", LogType.SYSTEM.toString()).replace("%Priority%", String.valueOf(1)).replace("%Content%", content);
+        save(_content);
+    }
+
+    public void Warning(String content){
+        String _content = this.LogString.replace("%Date%", (new Date()).toString()).replace("%LogType%", LogType.WARNING.toString()).replace("%Priority%", String.valueOf(1)).replace("%Content%", content);
+        save(_content);
+    }
+
+    public void Fatal(String content){
+        String _content = this.LogString.replace("%Date%", (new Date()).toString()).replace("%LogType%", LogType.FATAL.toString()).replace("%Priority%", String.valueOf(8)).replace("%Content%", content);
+        save(_content);
+    }
+
+    public void Error(String content){
+        String _content = this.LogString.replace("%Date%", (new Date()).toString()).replace("%LogType%", LogType.ERROR.toString()).replace("%Priority%", String.valueOf(10)).replace("%Content%", content);
+        save(_content);
+    }
+
+    public void Error(Exception exception){
+        String _content = this.LogString.replace("%Date%", (new Date()).toString()).replace("%LogType%", LogType.SYSTEM.toString()).replace("%Priority%", String.valueOf(1)).replace("%Content%", exception.getMessage());
+        save(_content);
+    }
+
+    public void Log(Exception exception){
+        String _content = this.LogString.replace("%Date%", (new Date()).toString()).replace("%LogType%", LogType.ERROR.toString()).replace("%Priority%", String.valueOf(4)).replace("%Content%", exception.getMessage());
+        save(_content);
+    }
+
     public void Log(LogType logType, int priority, String content) {
         String _content = this.LogString.replace("%Date%", (new Date()).toString()).replace("%LogType%", logType.toString()).replace("%Priority%", String.valueOf(priority)).replace("%Content%", content);
+        save(_content);
+    }
+
+    private void save(String content){
         try {
             FileWriter fw = new FileWriter(this._file, true);
             BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(_content);
+            bw.write(content);
             bw.newLine();
             bw.close();
             System.out.println(content);
